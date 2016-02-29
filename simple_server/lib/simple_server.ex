367c7b2bbf6fe84f3a3b626a1fc5e0fdf6172321
Ville Heikkinen <vheikkinen@topcon.com>
2016-02-29 11:09:30 +0200

    Simple Cowboy server

---
+++ simple_server/lib/simple_server.ex
@@ -0,0 +1,17 @@
+defmodule SimpleServer do
+	use Application
+
+  def start(_type, _args) do
+
+  	port = Application.get_env(:simple_server, :SERVER_PORT)
+
+    dispatch = :cowboy_router.compile([
+                 {:_, [{"/", SimpleServer.PageHandler, []}]}
+               ])
+    {:ok, _} = :cowboy.start_http(:http, 100,
+                                  [port: port],
+                                  [env: [dispatch: dispatch]])
+    IO.puts("Server listening to port #{port}")
+    SimpleServer.Supervisor.start_link
+  end
+end
