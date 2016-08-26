fe524273d2379b926a938e69815832fe8c1d5a34
Ville Heikkinen <vheikkinen@topcon.com>
2016-08-26 10:19:12 +0300

    Update react, react-router versions, use react-router-scroll to fix scrolling to top on page change

--- elixir_web/app/js/routes.jsx
+++ elixir_web/app/js/routes.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Router, IndexRoute, Route, Link, hashHistory } from 'react-router'
+import { Router, IndexRoute, Route, Link, hashHistory, applyRouterMiddleware } from 'react-router'
+import {useScroll} from 'react-router-scroll';
 
 import Application from './components/Application.jsx'
 import Page from './components/Page.jsx'
@@ -8,19 +9,20 @@ import {chapters} from './chapters.js'
 
 export default class Routes extends React.Component {
 
-    render(){
+  render() {
 
-        let content = chapters.map(chapter => 
-          (<Route path={chapter.path} key={chapter.path} chapter={chapter} component={Page}/>))
+    let content = chapters.map(chapter =>
+      (<Route path={chapter.path} key={chapter.path} chapter={chapter} component={Page}/>))
 
-        return(
-            <Router history={hashHistory}>
-              <Route path="/" component={Application}>
-                <IndexRoute component={Contents} />
-                {content}
-                <Route path="*" component={Page}/>
-              </Route>
-            </Router>
-          );
-    }
+    return (
+      <Router history={hashHistory}
+        render={applyRouterMiddleware(useScroll()) }>
+        <Route path="/" component={Application}>
+          <IndexRoute component={Contents} />
+          {content}
+          <Route path="*" component={Page}/>
+        </Route>
+      </Router>
+    );
+  }
 }
